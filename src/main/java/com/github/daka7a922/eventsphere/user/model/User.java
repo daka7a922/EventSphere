68be3107cc47434d258cb8619692e18d5b41d964
Danny P <y.popov1516@gmail.com>
2025-03-21 13:20:57 +0200

    Update User.java

--- src/main/java/com/github/daka7a922/eventsphere/user/model/User.java
+++ src/main/java/com/github/daka7a922/eventsphere/user/model/User.java
@@ -1,8 +1,11 @@
 package com.github.daka7a922.eventsphere.user.model;
 
+import com.github.daka7a922.eventsphere.event.model.Event;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -17,16 +20,27 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
+    private String firstName;
+
+    private String lastName;
+
+    private String profilePicture;
+
+    @Column(nullable = false)
     private String password;
 
+    @Column(unique = true)
     private String email;
 
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
-    
+    private boolean isActive;
 
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Event> events = new ArrayList<>();
 
 }
