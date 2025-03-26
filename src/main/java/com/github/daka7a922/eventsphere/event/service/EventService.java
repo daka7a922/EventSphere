6050aa6226a7eb1a76902b90af57b44157a6f17e
Danny P <y.popov1516@gmail.com>
2025-03-26 14:30:34 +0200

    Update EventService.java

--- src/main/java/com/github/daka7a922/eventsphere/event/service/EventService.java
+++ src/main/java/com/github/daka7a922/eventsphere/event/service/EventService.java
@@ -1,7 +1,39 @@
 package com.github.daka7a922.eventsphere.event.service;
 
+import com.github.daka7a922.eventsphere.event.model.Event;
+import com.github.daka7a922.eventsphere.event.repository.EventRepository;
+import com.github.daka7a922.eventsphere.web.dto.CreateEventRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventService {
+
+    private final EventRepository eventRepository;
+
+    @Autowired
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+
+    public Event createEvent(CreateEventRequest createEventRequest) {
+
+
+        Event event = Event.builder()
+                .name(createEventRequest.getName())
+                .description(createEventRequest.getDescription())
+                .dateAndTime(createEventRequest.getDateAndTime())
+                .eventPicture(createEventRequest.getEventPictureURL())
+                .eventType(createEventRequest.getEventType())
+                .availableTickets(createEventRequest.getAvailableTickets())
+                .venue(createEventRequest.getVenue())
+                .build();
+
+
+        return eventRepository.save(event);
+
+
+    }
 }
