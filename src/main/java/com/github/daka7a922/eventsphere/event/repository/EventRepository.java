fa6608a591b9961d72ef2f1911be172d1e4a8c02
Danny P <y.popov1516@gmail.com>
2025-03-28 09:01:25 +0200

    Update EventRepository.java

--- src/main/java/com/github/daka7a922/eventsphere/event/repository/EventRepository.java
+++ src/main/java/com/github/daka7a922/eventsphere/event/repository/EventRepository.java
@@ -1,12 +1,16 @@
 package com.github.daka7a922.eventsphere.event.repository;
 
 import com.github.daka7a922.eventsphere.event.model.Event;
+import com.github.daka7a922.eventsphere.event.model.EventType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
     Event getEventById(UUID id);
+
+    List<Event> findByEventType(EventType eventType);
 }
